ac164f0d7d37241327cd1f707bdb9474a80b4cfb
Todor <todorgz7@gmail.com>
2021-11-01 00:36:44 +0200

    Add hangman progress visualization

--- lib/visualizer.rb
+++ lib/visualizer.rb
@@ -3,5 +3,53 @@ class Visualizer
     puts "Word: #{game_word}"
     puts "Guess: #{guess}"
     puts "Misses: #{misses.reduce { |product, n| product + " #{n}" }}"
+    draw_body(misses.length)
+  end
+
+  def draw_body(misses_length)
+    case misses_length
+    when 0
+      puts '-----|======'
+      puts '-----O----||'
+      puts '---/-|-\--||'
+      puts '----/-\---||'
+      puts '__________||'
+    when 1
+      puts '-----|======'
+      puts '-----O----||'
+      puts '----------||'
+      puts '----------||'
+      puts '__________||'
+    when 2
+      puts '-----|======'
+      puts '-----O----||'
+      puts '-----|----||'
+      puts '----------||'
+      puts '__________||'
+    when 3
+      puts '-----|======='
+      puts '-----O----||'
+      puts '---/-|----||'
+      puts '----------||'
+      puts '__________||'
+    when 4
+      puts '-----|======='
+      puts '-----O----||'
+      puts '---/-|-\--||'
+      puts '----------||'
+      puts '__________||'
+    when 5
+      puts '-----|======'
+      puts '-----O----||'
+      puts '---/-|-\--||'
+      puts '----/-----||'
+      puts '___/______||'
+    when 6
+      puts '-----|======'
+      puts '-----O----||'
+      puts '---/-|-\--||'
+      puts '----/-\---||'
+      puts '___/___\__||'
+    end
   end
 end
